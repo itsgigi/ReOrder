9b6bac3ba7c06876777971b1eab6f6ed86d5bdd3
itsgigi <luigi.dl@hotmail.it>
2024-04-11 10:55:57 +0200

    EventPreview Updated

--- client/src/scenes/event/previewEvent/previewEventForm.tsx
+++ client/src/scenes/event/previewEvent/previewEventForm.tsx
@@ -1,11 +1,7 @@
 import { Button, Input, Typography } from "@mui/material";
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { useNavigate } from "react-router-dom";
 import { GetEventResponse } from "@/state/types";
-import { Dayjs } from "dayjs";
+import moment from "moment";
 
 type PreviewEventFormProps = {
     eventData: GetEventResponse
@@ -17,22 +13,12 @@ const PreviewEventForm = ({eventData}: PreviewEventFormProps) => {
     return (
       <>
           <div style={{backgroundColor: "#ced3dc", borderRadius: '10px', display: 'flex', flexDirection: 'column', gap: 12, height: 'max-content', padding: 16, width: '100%', boxShadow: "0.1rem 0.15rem 0.1rem 0.1rem rgba(67, 112, 133, 0.9)"}}>
-              <Typography style={{fontSize: 16, color: '#043028', marginBottom: 8}}>Aggiungi Nuova Azienda</Typography>
+              <Typography style={{fontSize: 16, color: '#043028', marginBottom: 8}}>Dettagli dell'evento</Typography>
   
               <Typography style={{fontSize: 14, color: '#043028'}}>Nome</Typography>
               <Input style={{fontSize: 14, color: '#4e8098'}} placeholder="Nome per identificare l'evento" value={eventData.name} /* onChange={e => setName(e.target.value)} */ />
               <Typography style={{fontSize: 14, color: '#043028'}}>Data</Typography>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DemoContainer components={['DatePicker', 'DatePicker']}>
-                    <DatePicker
-                        label="Data dell'evento"
-                        value={eventData.date as unknown as Dayjs}
-                        format="DD-MM-YYYY"
-                        disabled
-                        //onChange={(newValue) => setDate(newValue as any)}
-                    />
-                </DemoContainer>
-              </LocalizationProvider>
+              <Input type="date" style={{fontSize: 14, color: '#4e8098', marginBottom: 8}} placeholder="Numero persone previste" value={moment(eventData.date).format('YYYY-MM-DD')} disabled />
               <Typography style={{fontSize: 14, color: '#043028'}}>Persone</Typography>
               <Input type="number" style={{fontSize: 14, color: '#4e8098', marginBottom: 8}} placeholder="Numero persone previste" value={eventData.pax} disabled /*  onChange={e => setPax(parseInt(e.target.value))} */ />
   
