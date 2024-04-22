ba880163462a88583ca27ec2a345cae7af8688af
itsgigi <luigi.dl@hotmail.it>
2024-04-22 19:38:04 +0200

    2try passw

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,22 +7,32 @@ const router = express.Router();
 
 router.post("/users", async (req, res) => {
   try {
-    const {email} = req.body;
+    const {email, password} = req.body;
     const user = await User.findOne({email});
 
     if(!user) {
         return res.json({message: 'Utente non trovato', status: 401})
     }
 
-    const validPassword = await bcrypt.compare(password, user.password);
+    const validPassword = bcrypt.compare(password, user.password, function (err, res) {
+      if (err) {
+        console.warn('[Login] err ->', err);
+      }
+      if (res) {
+        // Send JWT
+        const token = jwt.sign({ username: user.username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' });
+        res.cookie('token', token, { httpOnly: true, maxAge: 2880000 });
+
+        res.status(200).json({ status: 200 });
+      } else {
+        // response is OutgoingMessage object that server response http request
+        return response.json({ status: 400, message: 'Password errata' });
+      }
+    })
     if(!validPassword) {
         return res.json({message: 'Password errata'});
     }
 
-    const token = jwt.sign({username: user.username}, 'jwttokenkey123encrp../$$1%unique.', {expiresIn: '8h'});
-    res.cookie('token', token, {httpOnly: true, maxAge: 2880000});
-
-    res.status(200).json({status: 200});
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
