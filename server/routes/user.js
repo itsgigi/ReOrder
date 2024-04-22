fc58ce99e7d37826a0d544d257eb0cccee892d59
itsgigi <luigi.dl@hotmail.it>
2024-04-22 20:24:32 +0200

    dedaaa

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,31 +8,29 @@ const router = express.Router();
 router.post("/users", async (req, res) => {
   try {
     const {email, password} = req.body;
-    const user = await User.findOne({email}).then((isOk) => {
-      if(isOk) {
-        const isValid = bcrypt.compare(password, user.password, function (err, valid) {
-          if (err) {
-            console.warn('[Login] err ->', err);
-          }
-          if (valid) {
-            // Send JWT
-            const token = jwt.sign({ username: user.username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' })
-            res.cookie('token', token, { httpOnly: true, maxAge: 2880000 })
-    
-            return res.status(200).json({ status: 200 , message: 'Login effettuato' });
-          } else {
-            // response is OutgoingMessage object that server response http request
-            return res.json({ status: 400, message: 'Password errata' });
-          }
-        });
-        if(!isValid) {
-          return res.json({ status: 400, message: 'Password errata' });
-        }
-      }
-    })
+    const user = await User.findOne({email})
 
     if(!user) {
         return res.json({message: 'Utente non trovato', status: 401})
+    }
+
+    const isValid = bcrypt.compare(password, user.password, function (err, valid) {
+      if (err) {
+        console.warn('[Login] err ->', err);
+      }
+      if (valid) {
+        // Send JWT
+        const token = jwt.sign({ username: user.username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' })
+        res.cookie('token', token, { httpOnly: true, maxAge: 2880000 })
+
+        return res.status(200).json({ status: 200 , message: 'Login effettuato' });
+      } else {
+        // response is OutgoingMessage object that server response http request
+        return res.json({ status: 400, message: 'Password è errata' });
+      }
+    });
+    if(!isValid) {
+      return res.json({ status: 400, message: 'Password errata' });
     }
 
   } catch (error) {
