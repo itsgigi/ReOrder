afebf5f2b1766e4cf0628a3fc57b734b786cb1a2
itsgigi <luigi.dl@hotmail.it>
2024-04-22 19:52:56 +0200

    online try

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,20 +7,20 @@ const router = express.Router();
 
 router.post("/users", async (req, res) => {
   try {
-    const {email} = req.body;
+    const {email, password} = req.body;
     const user = await User.findOne({email});
 
     if(!user) {
         return res.json({message: 'Utente non trovato', status: 401})
     }
 
-    const validPassword = bcrypt.compare(req.body.password, user.password, function (err, res) {
+    const validPassword = bcrypt.compare(password, user[0].password, function (err, res) {
       if (err) {
         console.warn('[Login] err ->', err);
       }
       if (res) {
         // Send JWT
-        const token = jwt.sign({ username: user.username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' });
+        const token = jwt.sign({ username: user[0].username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' });
         res.cookie('token', token, { httpOnly: true, maxAge: 2880000 });
 
         res.status(200).json({ status: 200 });
