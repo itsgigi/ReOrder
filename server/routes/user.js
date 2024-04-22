d9941be22ccff69d917214a064fbda9b892f834e
itsgigi <luigi.dl@hotmail.it>
2024-04-22 20:15:30 +0200

    dedd

--- server/routes/user.js
+++ server/routes/user.js
@@ -13,24 +13,25 @@ router.post("/users", async (req, res) => {
     if(!user) {
         return res.json({message: 'Utente non trovato', status: 401})
     }
-    request.server.log(user, password, user.password)
 
-    const isValid = bcrypt.compare(password, user.password, function (err, res) {
+    const isValid = await bcrypt.compare(password, user.password, function (err, res) {
       if (err) {
         console.warn('[Login] err ->', err);
       }
       if (res) {
         // Send JWT
-        const token = jwt.sign({ username: user.username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' });
-        res.cookie('token', token, { httpOnly: true, maxAge: 2880000 });
+        const token = jwt.sign({ username: user.username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' })
+        res.cookie('token', token, { httpOnly: true, maxAge: 2880000 })
 
-        res.status(200).json({ status: 200 });
+        return res.status(200).json({ status: 200 , message: 'Login effettuato' });
       } else {
         // response is OutgoingMessage object that server response http request
         return response.json({ status: 400, message: 'Password errata' });
       }
     });
-    request.server.log('is valid',isValid)
+    if(!isValid) {
+      return response.json({ status: 400, message: 'Password errata' });
+    }
 
   } catch (error) {
     res.status(404).json({ message: error.message });
