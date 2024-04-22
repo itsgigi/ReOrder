41996d1fd862a48a391f65591817217d372d5024
itsgigi <luigi.dl@hotmail.it>
2024-04-22 19:59:36 +0200

    rollback

--- server/routes/user.js
+++ server/routes/user.js
@@ -14,13 +14,13 @@ router.post("/users", async (req, res) => {
         return res.json({message: 'Utente non trovato', status: 401})
     }
 
-    const validPassword = bcrypt.compare(password, user[0].password, function (err, res) {
+    const validPassword = bcrypt.compare(password, user.password, function (err, res) {
       if (err) {
         console.warn('[Login] err ->', err);
       }
       if (res) {
         // Send JWT
-        const token = jwt.sign({ username: user[0].username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' });
+        const token = jwt.sign({ username: user.username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' });
         res.cookie('token', token, { httpOnly: true, maxAge: 2880000 });
 
         res.status(200).json({ status: 200 });
