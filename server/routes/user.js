5c20a88888afb931f0d3853b4e0d65b22d1f3592
itsgigi <luigi.dl@hotmail.it>
2024-04-22 20:16:37 +0200

    valid

--- server/routes/user.js
+++ server/routes/user.js
@@ -14,11 +14,11 @@ router.post("/users", async (req, res) => {
         return res.json({message: 'Utente non trovato', status: 401})
     }
 
-    const isValid = await bcrypt.compare(password, user.password, function (err, res) {
+    const isValid = await bcrypt.compare(password, user.password, function (err, valid) {
       if (err) {
         console.warn('[Login] err ->', err);
       }
-      if (res) {
+      if (valid) {
         // Send JWT
         const token = jwt.sign({ username: user.username }, 'jwttokenkey123encrp../$$1%unique.', { expiresIn: '8h' })
         res.cookie('token', token, { httpOnly: true, maxAge: 2880000 })
@@ -26,7 +26,7 @@ router.post("/users", async (req, res) => {
         return res.status(200).json({ status: 200 , message: 'Login effettuato' });
       } else {
         // response is OutgoingMessage object that server response http request
-        return response.json({ status: 400, message: 'Password errata' });
+        return res.json({ status: 400, message: 'Password errata' });
       }
     });
     if(!isValid) {
